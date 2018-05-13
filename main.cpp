11c195b597cc21aa9666b39776e9b00befed153f
jakubskalak <jakub.skalak@gmail.com>
2018-05-13 17:27:35 +0200

    zamiana na milisekundy

--- main.cpp
+++ main.cpp
@@ -51,7 +51,7 @@ return -1;
     MPI_Finalize();
 	high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
-	auto duration = duration_cast<microseconds>(t2 - t1).count();
+	auto duration = duration_cast<milliseconds>(t2 - t1).count();
 
 	cout << duration;
 
