54068e5701680b338162ca4496c35c1b0f6bb4aa
jakubskalak <jakub.skalak@gmail.com>
2018-05-13 22:05:06 +0200

    test

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -102,14 +102,9 @@ void dijkstraMain(const Graph *graph, const std::string& initialNodeName, const 
 
 
             std::cout << "Total cost: " << distances[goalNode] << std::endl;
-
-			/*            std::cout << "Path: " << nodes[currentNode];
-
-            for(auto it=stack.rbegin(); it != stack.rend(); ++it) {
-                auto nextNodeName = nodes[*it];
-                std::cout << " -> " << nextNodeName;
-				std::cout << std::endl;
-            }*/
+			for (int d : distances) {
+				std::cout << d;
+			}
 
 			std::cout << "Result path: " << nodes[currentNode];
 			std::reverse(stack.begin(), stack.end());
